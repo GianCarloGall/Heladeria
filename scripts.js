aa7a2373c0393687cf49d664527f7de61bd08798
GianCarloGall <ggallegosg1900@alumno.ipn.mx>
2021-04-26 00:58:57 -0500

    Pruebas

--- scripts.js
+++ scripts.js
@@ -13,3 +13,21 @@ jQuery('document').ready(function($){
         }
     });
 });
+function Inicio(){
+    location.href = "index.html"
+}
+function IniciarSesion(){
+    location.href = "IniciarSesion.html";
+}
+function CrearCuenta(){
+    location.href = "CrearCuenta.html";
+}
+function Olvidar(){
+    location.href = "Olvidar.html";
+}
+function abrir(){
+    document.getElementById("vent").style.display="block";
+}
+function cerrar(){
+    document.getElementById("vent").style.display="none";
+}
